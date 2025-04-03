9a4764a049c99119a04bd35937ca4bedad9d4abd
Your Name <you@example.com>
2025-04-03 21:07:27 +0800

    feat: 添加配音页面功能，包括视频播放和字幕显示

---
+++ apps/view/src/modules/dub-videos/video-resource.ts
@@ -0,0 +1,12 @@
+export const DUB_VIDEOS = [
+  {
+    type: 'dub',
+    title: "青春变形记",
+    desc: "青春变形记",
+    author: {
+      avatar: "https://img2.quotawish.com/2025/01/30/679b1932e7dfe.png",
+      name: "disney"
+    },
+    url: "/modules/dub-videos/video1.mp4" // 确保路径正确
+  }
+]
