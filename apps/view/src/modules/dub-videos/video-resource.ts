84a61b69b6deabd77561b9682aeed3cc4f66cf7e
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2025-04-06 02:51:14 +0800

    feat: 删除不再使用的视频文件，更新视频资源路径，优化视频播放逻辑以提升代码可维护性

--- apps/view/src/modules/dub-videos/video-resource.ts
+++ apps/view/src/modules/dub-videos/video-resource.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-=======
 /**
  * 配音视频资源
  * https://img2.quotawish.com/8c0350dd30e266681b2ef7b9b3344642.mp4
  */
->>>>>>> c690622aed9868d7e15b1acafefee3230924cc74
 export const DUB_VIDEOS = [
   {
     type: 'dub',
@@ -12,12 +9,9 @@ export const DUB_VIDEOS = [
     desc: "青春变形记",
     author: {
       avatar: "https://img2.quotawish.com/2025/01/30/679b1932e7dfe.png",
-      name: "disney"
+      name: "disney",
     },
-<<<<<<< HEAD
-    url: "/modules/dub-videos/video1.mp4" // 确保路径正确
-=======
-    url: "https://img2.quotawish.com/8c0350dd30e266681b2ef7b9b3344642.mp4"
->>>>>>> c690622aed9868d7e15b1acafefee3230924cc74
-  }
+    url: "/modules/dub-videos/video1.mp4",
+    // url: "https://img2.quotawish.com/8c0350dd30e266681b2ef7b9b3344642.mp4",
+  },
 ]
