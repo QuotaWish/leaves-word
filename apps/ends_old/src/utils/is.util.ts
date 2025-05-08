e4a24efbdc23b1b89341f706c0355bb8d6300fc1
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2025-05-08 21:03:51 +0800

    chore: delete duplicate impl ends

--- apps/ends_old/src/utils/is.util.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable regexp/no-useless-quantifier */
-export function isExternal(path: string): boolean {
-  // eslint-disable-next-line regexp/no-super-linear-backtracking
-  const reg = /(?:^https?:(?:\/\/)?(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+(?::\d+)?|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)(?:(?:\/[+~%/.\w-]*)?\??[-+=&;%@.\w]*(?:#\w*)?)?$/
-  return reg.test(path)
-}
