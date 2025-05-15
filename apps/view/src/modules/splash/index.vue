f36abf342797a8dcf3d48496faf2e29f02631b9e
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2025-05-15 20:11:15 +0800

    fix(view): 修复移动端屏幕适配逻辑，优化初始化方法调用格式，统一分号使用规范

--- apps/view/src/modules/splash/index.vue
+++ apps/view/src/modules/splash/index.vue
@@ -1,60 +1,59 @@
 <script setup lang="ts">
-import SpalshContainer from '~/modules/splash/SplashContainer.vue'
-import SplashLoading from '~/modules/splash/SplashLoading.vue'
-import { ScreenMode, useGlobalSplashState, useBuilder } from '.'
-import TheFooter from './footer/TheFooter.vue'
-import SplashLayout from './SplashLayout.vue'
-import SplashMenu from './SplashMenu.vue'
-import { useDeviceUaParser } from './ua-parser'
-import { useUniApp } from '~/composables/adapter/uniapp'
-import { useWebSizeAdapter } from '~/composables/adapter/web'
+import SpalshContainer from "~/modules/splash/SplashContainer.vue";
+import SplashLoading from "~/modules/splash/SplashLoading.vue";
+import { ScreenMode, useGlobalSplashState, useBuilder } from ".";
+import TheFooter from "./footer/TheFooter.vue";
+import SplashLayout from "./SplashLayout.vue";
+import SplashMenu from "./SplashMenu.vue";
+import { useDeviceUaParser } from "./ua-parser";
+import { useUniApp } from "~/composables/adapter/uniapp";
+import { useWebSizeAdapter } from "~/composables/adapter/web";
 
-const { isBuilder, check } = useBuilder()
-const { init, destroy } = useUniApp()
-const spalshState = useGlobalSplashState()
+const { isBuilder, check } = useBuilder();
+const { init, destroy } = useUniApp();
+const spalshState = useGlobalSplashState();
 
 function checkScreenSize() {
-  const { isBrowser } = useDeviceUaParser()
+  const { isBrowser } = useDeviceUaParser();
 
-  check()
+  check();
   init(() => {
-    console.log('init')
-  })
+    console.log("init");
+  });
   onBeforeUnmount(() => {
-    destroy()
-  })
+    destroy();
+  });
 
   if (window.innerWidth > 768) {
-    spalshState.screenMode.value = ScreenMode.WRAPPED
-    document.body.classList.add('large-screen')
-  }
-  else {
-    document.body.classList.remove('large-screen')
+    spalshState.screenMode.value = ScreenMode.WRAPPED;
+    document.body.classList.add("large-screen");
+  } else {
+    document.body.classList.remove("large-screen");
 
     if (isBuilder.value) {
-      spalshState.screenMode.value = ScreenMode.BUILDER
+      spalshState.screenMode.value = ScreenMode.BUILDER;
+    } else {
+      spalshState.screenMode.value = ScreenMode.MOBILE;
     }
-    else {
-      spalshState.screenMode.value = ScreenMode.MOBILE
 
-      useWebSizeAdapter()
-    }
+    useWebSizeAdapter();
   }
 
-  spalshState.mockStatusbar.value = isBrowser && spalshState.screenMode.value === ScreenMode.WRAPPED
+  spalshState.mockStatusbar.value =
+    isBrowser && spalshState.screenMode.value === ScreenMode.WRAPPED;
 }
 
-const { width, height } = useWindowSize()
+const { width, height } = useWindowSize();
 
 watch([width, height], () => {
-  nextTick(checkScreenSize)
-})
+  nextTick(checkScreenSize);
+});
 
 setTimeout(() => {
-  checkScreenSize()
+  checkScreenSize();
 
-  document.body.classList.add('mobile')
-}, 300)
+  document.body.classList.add("mobile");
+}, 300);
 </script>
 
 <template>
